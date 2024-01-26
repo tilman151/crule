983c35c0c917e1e67c9e7a7d2a0848b5f712ffa1
Tilman Krokotsch <t.krokotsch@gmail.com>
2024-01-26 17:00:17 +0100

    feat: add tuning script for two-stage extractor

--- crule/run/utils.py
+++ crule/run/utils.py
@@ -16,3 +16,39 @@ class XjtuSyExtractor:
         targets = targets.repeat(num_slices)
 
         return features, targets
+
+
+class XjtuSyWindowExtractor:
+    def __init__(self, upper_window_size=10, lower_window_size=2560):
+        self.upper_window_size = upper_window_size
+        self.lower_window_size = lower_window_size
+
+    def __call__(self, features, targets):
+        num_slices = features.shape[1] // self.lower_window_size
+        num_features = features.shape[-1]
+        cutoff = num_slices * self.lower_window_size
+        features = features[:, :cutoff].reshape(
+            -1, self.lower_window_size, num_features
+        )
+        features = self.extract_windows(features, self.upper_window_size, num_slices)
+        window_cutoff = self._cutoff(self.upper_window_size, num_slices)
+        targets = targets.repeat(num_slices)[window_cutoff:]
+
+        return features, targets
+
+    def extract_windows(self, seq, window_size, dilation):
+        if window_size * dilation > len(seq):
+            raise ValueError(
+                f"Cannot extract windows of size {window_size} with stride {dilation} "
+                f"from a sequence of length {len(seq)}."
+            )
+
+        num_frames = seq.shape[0] - self._cutoff(window_size, dilation)
+        window_idx = np.arange(window_size)[None, :] * dilation
+        window_idx = window_idx + np.arange(num_frames)[:, None]
+        windows = seq[window_idx]
+
+        return windows
+
+    def _cutoff(self, window_size, dilation):
+        return (window_size - 1) * dilation
