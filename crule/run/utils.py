1a2a357a7b3ea40a707817d3c6a8e78e76af8fe0
Tilman Krokotsch <t.krokotsch@gmail.com>
2024-02-02 08:59:33 +0100

    feat: implement memory-saving feature extractor

--- crule/run/utils.py
+++ crule/run/utils.py
@@ -27,32 +27,13 @@ class XjtuSyWindowExtractor:
         features = features.astype(np.float16)
         num_slices = features.shape[1] // self.lower_window_size
         num_features = features.shape[-1]
-        cutoff = num_slices * self.lower_window_size
-        features = features[:, :cutoff].reshape(
-            -1, self.lower_window_size, num_features
-        )
-        features = self.extract_windows(features, self.upper_window_size, num_slices)
-        window_cutoff = self._cutoff(self.upper_window_size, num_slices)
+        window_shape = (self.upper_window_size, self.lower_window_size, num_features)
+        features = np.lib.stride_tricks.sliding_window_view(features, window_shape)
+        features = features[:, :: self.lower_window_size]  # tumbling over lower windows
+        window_cutoff = (self.upper_window_size - 1) * num_slices
         targets = targets.repeat(num_slices)[window_cutoff:]
 
         return features, targets
-
-    def extract_windows(self, seq, window_size, dilation):
-        if window_size * dilation > len(seq):
-            raise ValueError(
-                f"Cannot extract windows of size {window_size} with stride {dilation} "
-                f"from a sequence of length {len(seq)}."
-            )
-
-        num_frames = seq.shape[0] - self._cutoff(window_size, dilation)
-        window_idx = np.arange(window_size, dtype=np.uint16)[None, :] * dilation
-        window_idx = window_idx + np.arange(num_frames, dtype=np.uint16)[:, None]
-        windows = seq[window_idx]
-
-        return windows
-
-    def _cutoff(self, window_size, dilation):
-        return (window_size - 1) * dilation
 
     def __repr__(self):
         return (
