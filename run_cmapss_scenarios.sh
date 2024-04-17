4e47163b791c68970d2c81ccdb127a63be63ae8b
Tilman Krokotsch <t.krokotsch@gmail.com>
2024-04-17 12:25:54 +0200

    feat: add run script to replicate data scenario experiments

---
+++ run_cmapss_scenarios.sh
@@ -0,0 +1,27 @@
+ENTITY=tkrokotsch
+PROJECT=data-scenarios
+APPROACHES=("conditional_dann" "latent_align" "pseudo_labels")
+PERCENT_BROKEN=("1.0" "0.8" "0.6" "0.4" "0.2")
+PERCENT_FAILED=("1.0" "0.8" "0.6" "0.4" "0.2")
+REPLICATIONS=5
+
+for APPROACH in "${APPROACHES[@]}"; do
+for BROKEN in "${PERCENT_BROKEN[@]}"; do
+for FAILED in "${PERCENT_FAILED[@]}"; do
+poetry run python train.py \
+        --multirun hydra/launcher=ray \
+        +hydra.launcher.num_gpus=0.25 \
+        +task="glob(*)" \
+        +approach="$APPROACH" \
+        +feature_extractor=cnn \
+        +dataset=cmapss \
+        ++target.reader.percent_broken="$BROKEN" \
+        ++target.reader.percent_fail_runs="$FAILED" \
+        test=True \
+        logger.entity=$ENTITY \
+        logger.project=$PROJECT \
+       +logger.tags="[cmapss,broken-$BROKEN,failed-$FAILED]" \
+        replications=$REPLICATIONS
+done
+done
+done
