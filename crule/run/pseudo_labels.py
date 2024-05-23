20e8c5f419ab56fa21096931c7a72730995f5de0
Tilman Krokotsch <t.krokotsch@gmail.com>
2024-05-23 11:17:13 +0200

    feat: make running multiple pseudo-label iterations possible

--- crule/run/pseudo_labels.py
+++ crule/run/pseudo_labels.py
@@ -1,5 +1,7 @@
 from typing import Any, Dict
 
+import torch
+from pytorch_lightning import Trainer
 from torch.utils.data import ConcatDataset, DataLoader
 
 import rul_adapt
@@ -13,7 +15,6 @@ def pseudo_labels(config: Dict[str, Any]):
     approach = common.get_approach(config)
     approach.set_model(best_pretrained.feature_extractor, best_pretrained.regressor)
 
-    combined_dl = _get_adaption_dataloader(approach, dm)
     trainer = common.get_trainer(config)
     if common.is_wandb_logger(trainer.logger):
         trainer.logger.experiment.define_metric(
@@ -21,11 +22,24 @@ def pseudo_labels(config: Dict[str, Any]):
         )
         trainer.logger.experiment.config["approach"] = "PseudoLabelsApproach"
         trainer.logger.log_hyperparams(dm.hparams)  # log manually as dm isn't used
-    trainer.fit(
-        approach,
-        train_dataloaders=combined_dl,
-        val_dataloaders=dm.target.val_dataloader(),
-    )
+
+    best_val_score = float("inf")
+    while True:
+        combined_dl = _get_adaption_dataloader(approach, dm)
+        trainer.fit(
+            approach,
+            train_dataloaders=combined_dl,
+            val_dataloaders=dm.target.val_dataloader(),
+        )
+        val_score = trainer.checkpoint_callback.best_model_score
+        if val_score >= best_val_score or abs(val_score - best_val_score) < 0.1:
+            break
+        best_val_score = val_score
+        best_checkpoint = trainer.checkpoint_callback.best_model_path
+        approach = type(approach).load_from_checkpoint(best_checkpoint)
+        dm = common.get_adaption_datamodule(config)
+        _reset_trainer(trainer)
+
     result = common.get_result(config, trainer, dm)
     if common.is_wandb_logger(trainer.logger):
         trainer.logger.experiment.finish()
@@ -33,8 +47,16 @@ def pseudo_labels(config: Dict[str, Any]):
     return result
 
 
+def _reset_trainer(trainer: Trainer):
+    trainer.should_stop = False
+    trainer.early_stopping_callback.best_score = torch.tensor(float("inf"))
+    trainer.early_stopping_callback.wait_count = 0
+
+
 def _get_adaption_dataloader(approach, dm):
     approach.cpu()
+    if not hasattr(dm.source, "_data"):  # check if setup was already performed
+        dm.setup()
     pseudo_rul = rul_adapt.approach.generate_pseudo_labels(
         dm.target, approach, dm.inductive
     )
